0666822705a77cd0b04953a49549116e89806b21
Zohaib Ayaz <zohaibmlik203@gmail.com>
2026-01-29 15:22:50 +0500

    Update Encode-decode-paswd.py

--- Encode-decode-paswd.py
+++ Encode-decode-paswd.py
@@ -1,14 +1,24 @@
 import base64
 
-def encodepass():
-  pass
+def encryptpass(password):
+  encode = base64.b64encode(password.encode())
+  print(f"Your encoded password is \'{encode}\' ")
 
-def decodepass():
-  pass
+def decodepass(password):
+  decode=base64.b64decode(password)
+  pass_decode=decode.decode()
+  print(pass_decode)
 
 print("What you want")
-print("1) Encode Password\n2)Decode password")
-print(" Chose no: ")
+print("1) Encode Password\n2) Decode password: \n")
+chose = input("Enter no: ")
 
-
+if chose == "1":
+    encode_pass=input("Enter password: ")
+    encryptpass(encode_pass)
+elif chose == "2":
+     decode_pass= input('Enter password: ')
+     decodepass(decode_pass)
+else:
+    print("Invalid input!")
 
