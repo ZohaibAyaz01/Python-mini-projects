8f916fbea597af0cf61bb1b3fba0892524a380eb
Zohaib Ayaz <zohaibmlik203@gmail.com>
2026-01-29 15:23:54 +0500

    Improve input handling for password encoding/decoding
    Refactor user input loop for encoding and decoding passwords.

--- Encode-decode-paswd.py
+++ Encode-decode-paswd.py
@@ -2,23 +2,25 @@ import base64
 
 def encryptpass(password):
   encode = base64.b64encode(password.encode())
-  print(f"Your encoded password is \'{encode}\' ")
+  print(f"Your encoded password is: {encode} ")
 
 def decodepass(password):
   decode=base64.b64decode(password)
   pass_decode=decode.decode()
-  print(pass_decode)
+  print(f"Your encoded password is: {pass_decode} ")
 
-print("What you want")
-print("1) Encode Password\n2) Decode password: \n")
-chose = input("Enter no: ")
+while True:
+    print("1) Encode Password\n2) Decode password \n3) Quit")
+    chose = input("\nEnter no: ")
 
-if chose == "1":
-    encode_pass=input("Enter password: ")
-    encryptpass(encode_pass)
-elif chose == "2":
-     decode_pass= input('Enter password: ')
-     decodepass(decode_pass)
-else:
-    print("Invalid input!")
-
+    if chose == "1":
+        encode_pass=input("Enter password: ")
+        encryptpass(encode_pass)
+    elif chose == "2":
+        decode_pass= input('Enter password: ')
+        decodepass(decode_pass)
+    elif chose == "3":
+        print("Thanks!")
+        break
+    else:
+        print("Invalid input!")
